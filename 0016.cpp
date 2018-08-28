16b8b046ead13bda0995e14720e3792f770a1ad1
Manza21 <supakornmanza21@gmail.com>
2018-08-28 08:47:07 +0700

    Update 0016.cpp

--- 0016.cpp
+++ 0016.cpp
@@ -1,8 +1,6 @@
 #include<stdio.h>
-int realmax(int a, int b){
-if(a>b) return a;
-else return b;
-}
+#include<algorithm>
+using namespace std;
 int score[3];
 int main(){
 int n;
@@ -14,7 +12,7 @@ for(int i=0;s[i]!='\0';i++){
      if(s[i]=="BABC"[i%4]) score[1]++;
      if(s[i]=="CCAABB"[i%6]) score[2]++;
     }
-    int f=realmax(score[0],realmax(score[1],score[2]));
+    int f=max(score[0],max(score[1],score[2]));
     printf("%d\n",f);
     if(score[0]==f) printf("Adrian\n");
     if(score[1]==f) printf("Bruno\n");
