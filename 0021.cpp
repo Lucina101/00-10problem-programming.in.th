b64e5dd4c6901740a4bbacde24f3418beb60bfbc
Manza21 <supakornmanza21@gmail.com>
2018-10-15 22:38:41 +0700

    Update 0021.cpp

--- 0021.cpp
+++ 0021.cpp
@@ -6,7 +6,6 @@ int main(){
     char code[101];
     gets(code);// use when we want to receive string with null character
     //a e i o u
-    int dd= strlen(code);
     for(i=0;code[i]!='\0';i++){
     printf("%c",code[i]);
     if(code[i]=='a'||code[i]=='e'||code[i]=='i'||code[i]=='o'||code[i]=='u')
